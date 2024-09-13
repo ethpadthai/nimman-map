d341e987d5731a38af5bbe3e01a79d922cbd4fb1
JFKongphop <kongphopleo@gmail.com>
2024-09-13 22:46:22 +0700

    init map

---
+++ map.js
@@ -0,0 +1,68 @@
+const imageUrl = 'Fantasy-map.jpg';
+const imageWidth = 20933;
+const imageHeight = 15700;
+const distanceFromCenter = 1000
+
+const map = L.map('map', {
+  crs: L.CRS.Simple,
+  minZoom: -2,
+  maxZoom: 10,
+  zoomControl: true,
+  attributionControl: false
+});
+
+let imageBounds = [[0, 0], [imageHeight, imageWidth]];
+let imageLayer = L.imageOverlay(imageUrl, imageBounds).addTo(map);
+
+const adjustMapView = () => {
+  let aspectRatioViewport = window.innerWidth / window.innerHeight;
+  let aspectRatioImage = imageWidth / imageHeight;
+
+  if (aspectRatioViewport > aspectRatioImage) {
+    let newHeight = imageWidth / aspectRatioViewport;
+    imageBounds = [[(imageHeight - newHeight) / 2, 0], [(imageHeight + newHeight) / 2, imageWidth]];
+  } else {
+    let newWidth = imageHeight * aspectRatioViewport;
+    imageBounds = [[0, (imageWidth - newWidth) / 2], [imageHeight, (imageWidth + newWidth) / 2]];
+  }
+
+  imageLayer.setBounds(imageBounds);
+  map.fitBounds(imageBounds);
+  map.setMaxBounds(imageBounds);
+}
+
+adjustMapView();
+
+window.addEventListener('resize', adjustMapView);
+
+const marker = L.marker([imageHeight / 2, imageWidth / 2], {
+  icon: L.icon({
+    iconUrl: 'https://unpkg.com/leaflet/dist/images/marker-icon.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34]
+  })
+}).addTo(map);
+
+marker.bindPopup('<b>Hello world!</b><br>I am a popup.').openPopup();
+
+const centerX = imageHeight / 2;
+const centerY = imageWidth / 2;
+
+const markers = [
+  { coords: [centerX - distanceFromCenter, centerY - distanceFromCenter], popup: '<b>Hello world!</b><br>I am a popup at Top-Left Marker.' },
+  { coords: [centerX - distanceFromCenter, centerY + distanceFromCenter], popup: '<b>Hello world!</b><br>I am a popup at Top-Right Marker.' },
+  { coords: [centerX + distanceFromCenter, centerY - distanceFromCenter], popup: '<b>Hello world!</b><br>I am a popup at Bottom-Left Marker.' },
+  { coords: [centerX + distanceFromCenter, centerY + distanceFromCenter], popup: '<b>Hello world!</b><br>I am a popup at Bottom-Right Marker.' },
+];
+
+markers.forEach(function(marker) {
+  L.marker(marker.coords, {
+    icon: L.icon({
+      iconUrl: 'https://unpkg.com/leaflet/dist/images/marker-icon.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34]
+    })
+  }).addTo(map).bindPopup(marker.popup).openPopup();
+});
