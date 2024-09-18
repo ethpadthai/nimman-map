c0eddb66a7e5a30d83419b80e710342bf296292b
JFKongphop <kongphopleo@gmail.com>
2024-09-18 23:22:45 +0700

    add the modal when click marker

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 const imageUrl = 'Fantasy-map.jpg';
 const imageWidth = 20933;
 const imageHeight = 15700;
-const distanceFromCenter = 1000
+const distanceFromCenter = 1000;
 
 const map = L.map('map', {
   crs: L.CRS.Simple,
@@ -31,13 +31,43 @@ const adjustMapView = () => {
   map.setMaxBounds(imageBounds);
 }
 
-adjustMapView();
+const hideLoadingScreen = () => {
+  document.getElementById('loading').style.display = 'none';
+};
 
+map.once('load', hideLoadingScreen); 
+
+adjustMapView();
 window.addEventListener('resize', adjustMapView);
 
-const iconUrl = 'https://unpkg.com/leaflet/dist/images/marker-icon.png'
+const iconUrl = 'https://unpkg.com/leaflet/dist/images/marker-icon.png';
 
-const marker = L.marker([imageHeight / 2, imageWidth / 2], {
+const modal = document.getElementById('myModal');
+const modalText = document.getElementById('modal-text');
+const span = document.getElementsByClassName('close')[0];
+
+if (modalText.innerText === '') {
+  modal.style.display = 'none'
+}
+
+const openModal = (text) => {
+  modal.style.display = 'flex';
+  modalText.innerHTML = text;
+};
+
+const closeModal = () => {
+  modal.style.display = 'none';
+};
+
+span.onclick = closeModal;
+
+window.onclick = (event) => {
+  if (event.target === modal) {
+    closeModal();
+  }
+};
+
+const centerMarker = L.marker([imageHeight / 2, imageWidth / 2], {
   icon: L.icon({
     iconUrl,
     iconSize: [25, 41],
@@ -46,19 +76,21 @@ const marker = L.marker([imageHeight / 2, imageWidth / 2], {
   })
 }).addTo(map);
 
-marker.bindPopup('<b>Hello world!</b><br>I am a popup.')// .openPopup();
+centerMarker.on('click', () => {
+  openModal('Hello world on center');
+});
 
 const centerX = imageHeight / 2;
 const centerY = imageWidth / 2;
 
 const markers = [
-  { coords: [centerX - distanceFromCenter, centerY - distanceFromCenter], popup: '<b>Hello world!</b><br>I am a popup at Top-Left Marker.' },
-  { coords: [centerX - distanceFromCenter, centerY + distanceFromCenter], popup: '<b>Hello world!</b><br>I am a popup at Top-Right Marker.' },
-  { coords: [centerX + distanceFromCenter, centerY - distanceFromCenter], popup: '<b>Hello world!</b><br>I am a popup at Bottom-Left Marker.' },
-  { coords: [centerX + distanceFromCenter, centerY + distanceFromCenter], popup: '<b>Hello world!</b><br>I am a popup at Bottom-Right Marker.' },
+  { coords: [centerX - distanceFromCenter, centerY - distanceFromCenter], context: '<b>Hello world!</b><br>I am a popup at Top-Left Marker.' },
+  { coords: [centerX - distanceFromCenter, centerY + distanceFromCenter], context: '<b>Hello world!</b><br>I am a popup at Top-Right Marker.' },
+  { coords: [centerX + distanceFromCenter, centerY - distanceFromCenter], context: '<b>Hello world!</b><br>I am a popup at Bottom-Left Marker.' },
+  { coords: [centerX + distanceFromCenter, centerY + distanceFromCenter], context: '<b>Hello world!</b><br>I am a popup at Bottom-Right Marker.' },
 ];
 
-markers.forEach(function(marker) {
+markers.forEach(marker => {
   L.marker(marker.coords, {
     icon: L.icon({
       iconUrl,
@@ -66,5 +98,5 @@ markers.forEach(function(marker) {
       iconAnchor: [12, 41],
       popupAnchor: [1, -34]
     })
-  }).addTo(map).bindPopup(marker.popup)// .openPopup();
+  }).addTo(map).on('click', () => openModal(marker.context));
 });
